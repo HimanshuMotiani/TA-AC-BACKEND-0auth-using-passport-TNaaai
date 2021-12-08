19bc1aa99c4cb79b0c5ed9b77bc1fd44ec8f1076
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-12-08 12:41:10 +0530

    check logged in user from middlewares

--- block-BNaaef/articleApp-github-auth/routes/articles.js
+++ block-BNaaef/articleApp-github-auth/routes/articles.js
@@ -1,34 +1,26 @@
 var express = require('express');
 var Article = require("../models/article");
 var Comment = require("../models/comment")
+var auth = require("../middlewares/auth");
 var router = express.Router();
 
 /* GET articles listing. */
-router.get('/', function(req, res, next) {
-    console.log(req.session.passport.user , req.session.userId);
-    if(req.session.userId || req.session.passport.user){
-        Article.find({},(err,articlesList)=>{
-            if(err) return next(err);
-            res.render("articles",{articlesList : articlesList });
-        })
-    }
-        req.flash("error","User is not logged in")
-        return res.redirect("/users/login")
-    
+router.get('/', auth.isUserLoggedIn, function (req, res, next) {
+    Article.find({}, (err, articlesList) => {
+        if (err) return next(err);
+        res.render("articles", { articlesList: articlesList });
+    })
 });
-router.get('/new', function(req, res, next) {
-    if(req.session.userId || req.session.passport.user){
-    return res.render("articleForm"); 
-    }
-    req.flash("error","User is not logged in")
-    return res.redirect("/users/login")
+router.get('/new', auth.isUserLoggedIn, function (req, res, next) {
+    res.render("articleForm");
+
 });
 //submit 
-router.post('/', function(req, res, next) {
-    var { title,description,author } = req.body;
-  Article.create(req.body,(err,articleAdd)=>{
-      res.redirect("/articles")
-})
+router.post('/', auth.isUserLoggedIn, function (req, res, next) {
+    var { title, description, author } = req.body;
+    Article.create(req.body, (err, articleAdd) => {
+        res.redirect("/articles")
+    })
 })
 // router.get('/:id', function(req, res, next) {
 //     var id = req.params.id;
@@ -42,48 +34,37 @@ router.post('/', function(req, res, next) {
 
 // or using populate
 // get detail
-router.get('/:id', function(req, res, next) {
-    if(!req.session.userId){
-        req.flash("error","User is not logged in")
-        return res.redirect("/users/login")
-    }
+router.get('/:id',auth.isUserLoggedIn, function (req, res, next) {
     var id = req.params.id;
     Article.findById(id
-        ).populate('comments').exec((err,article)=>{
-        if(err) return next(err);
-            res.render("articlesDetail",{article})
-        })
-  });
+    ).populate('comments').exec((err, article) => {
+        if (err) return next(err);
+        console.log(article);
+        res.render("articlesDetail", { article })
+    })
+});
 //edit
-router.get('/:id/edit', function(req, res, next) {
-    if(!req.session.userId){
-        req.flash("error","User is not logged in")
-        return res.redirect("/users/login")
-    }
+router.get('/:id/edit',auth.isUserLoggedIn, function (req, res, next) {
     var id = req.params.id;
-    Article.findById(id,(err,article)=>{
-        if(err) return next(err);
-        res.render("articleUpdateForm",{article:article})
-  })
+    Article.findById(id, (err, article) => {
+        if (err) return next(err);
+        res.render("articleUpdateForm", { article: article })
+    })
 });
 //update 
-router.post('/:id', function(req, res, next) {
+router.post('/:id',auth.isUserLoggedIn, function (req, res, next) {
     var id = req.params.id;
-    Article.findByIdAndUpdate(id,req.body,(err,article)=>{
-        if(err) return next(err);
+    Article.findByIdAndUpdate(id, req.body, (err, article) => {
+        if (err) return next(err);
         res.redirect("/articles")
-  })
+    })
 });
 //delete
-router.get("/:id/delete", (req, res) => {
-    if(!req.session.userId){
-        req.flash("error","User is not logged in,log in first")
-        return res.redirect("/users/login")
-    }
+router.get("/:id/delete",auth.isUserLoggedIn, (req, res) => {
     var id = req.params.id;
     Article.findByIdAndDelete(id, (err, article) => {
         if (err) return next(err);
-        Comment.deleteMany({articleId:article.id},(err,comment)=>{
+        Comment.deleteMany({ articleId: article.id }, (err, comment) => {
             res.redirect("/articles")
         })
     })
@@ -91,30 +72,30 @@ router.get("/:id/delete", (req, res) => {
 
 //like
 router.get("/:id/likes", (req, res) => {
-    if(!req.session.userId){
-        req.flash("error","User is not logged in")
+    if (!req.session.userId) {
+        req.flash("error", "User is not logged in")
         return res.redirect("/users/login")
     }
     var id = req.params.id;
-    Article.findByIdAndUpdate(id,{$inc:{likes:1}} ,(err, article) => {
+    Article.findByIdAndUpdate(id, { $inc: { likes: 1 } }, (err, article) => {
         if (err) return next(err);
-        res.redirect("/articles/"+id)
+        res.redirect("/articles/" + id)
     })
 })
 
 //comments
 router.post("/:id/comments", (req, res) => {
-    if(!req.session.userId){
-        req.flash("error","User is not logged in")
+    if (!req.session.userId) {
+        req.flash("error", "User is not logged in")
         return res.redirect("/users/login")
     }
     var id = req.params.id;
     req.body.articleId = id;
-    Comment.create(req.body,(err,comment)=>{
+    Comment.create(req.body, (err, comment) => {
         if (err) return next(err);
-        Article.findByIdAndUpdate(id,{$push:{comments:comment.id}},(err,comment)=>{
-            if (err) return next(err);  
-            res.redirect("/articles/"+ id)
+        Article.findByIdAndUpdate(id, { $push: { comments: comment.id } }, (err, comment) => {
+            if (err) return next(err);
+            res.redirect("/articles/" + id)
         })
     })
 })
